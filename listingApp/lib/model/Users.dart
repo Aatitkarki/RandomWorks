f98cfe7dd1b1b725c683c44034c91001c732fbe3
Aatitkarki <52074509+Aatitkarki@users.noreply.github.com>
2020-08-27 14:12:22 +0545

    latest

---
+++ listingApp/lib/model/Users.dart
@@ -0,0 +1,7 @@
+class User {
+  String name;
+  String username;
+  String email;
+  String phone;
+  String website;
+}
--- listingApp/lib/services/Api.dart
+++ listingApp/lib/services/Api.dart
@@ -1,1 +1,4 @@
 //TODO: write the api links
+//https://jsonplaceholder.typicode.com/users/
+//https://jsonplaceholder.typicode.com/users/{user_id}/posts
+//https://jsonplaceholder.typicode.com/posts/{post_id}/comments
