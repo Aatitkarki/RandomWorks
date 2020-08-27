8cb33fefcc900e1d0d2b334e470ed2f64b762301
Aatitkarki <52074509+Aatitkarki@users.noreply.github.com>
2020-08-27 15:41:36 +0545

    latest

--- listingApp/lib/services/Api.dart
+++ listingApp/lib/services/Api.dart
@@ -2,3 +2,13 @@
 //https://jsonplaceholder.typicode.com/users/
 //https://jsonplaceholder.typicode.com/users/{user_id}/posts
 //https://jsonplaceholder.typicode.com/posts/{post_id}/comments
+
+import 'package:http/http.dart' as http;
+
+class ApiServices {
+  String userAPI = "https://jsonplaceholder.typicode.com/users/";
+  Future<void> getUserData() {
+    var response = http.get(userAPI);
+    print(response);
+  }
+}
--- listingApp/lib/ui/test.dart
+++ listingApp/lib/ui/test.dart
@@ -1,1 +1,10 @@
-//TODO: TESTING
+// TODO: TESTING
+
+import 'package:flutter/material.dart';
+
+class MyFile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
