18414db1e078c6b91eb97fc5f32c37504c753e11
Jagadish <shresthak661@gmail.com>
2020-09-07 10:24:42 +0545

    Designs

--- listingApp/android/app/src/main/kotlin/com/example/listingApp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.listingApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- listingApp/lib/Prefs keys/keys.dart
+++
@@ -1,5 +0,0 @@
-const String users = "Users";
-
-const String posts = "Posts";
-
-const String comments = "comments";
